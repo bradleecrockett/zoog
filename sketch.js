f60b323cb516f819ed787e947a7069c988f44470
Brad <bcrockett@rjuhsd.us>
2024-01-29 11:41:27 -0800

    initial commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+// Name:
+// Date:
+
+
+function setup() {
+	//This function get run once at the start of the program
+	createCanvas(400, 400);
+}
+
+function draw() {
+	//This loop gets run over and over again
+	background(220);
+
+}
